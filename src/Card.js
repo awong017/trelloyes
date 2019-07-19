129c85443c1e16f4c70bf5422d12e84d4bd64356
Adam Wong <awong017@ucr.edu>
2019-07-19 11:33:32 -0700

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './Card.css';
+
+export default function Card(props) {
+    console.log(props);
+  return (
+    <div className='Card'>
+      <button
+        type='button'
+      >
+        delete
+      </button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  )
+}
